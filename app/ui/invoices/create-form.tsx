0564dbd174617e98ce54b199f40084bccf6beab3
Joseph-DD2 <catce.2024111tads0038@aluno.ifpi.edu.br>
2025-12-08 00:10:14 -0300

    ...

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -16,7 +16,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
   };
   
   return (
-    <form action={create}>
+    <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
